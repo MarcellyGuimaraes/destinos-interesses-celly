ff16fdaea3d3bc778e1e867106fc2905be26fd0f
IsisG13 <isissouzaguimaraes@gmail.com>
2022-10-28 20:36:22 -0300

    add multiple select

--- src/components/Forms/Select/index.jsx
+++ src/components/Forms/Select/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Label from '../Label'
 import { useCountry } from '../api/fetchApi'
+import Select from 'react-select'
 
 export const SelectCity = (label) => {
   const city = useCountry('city')
@@ -9,12 +10,12 @@ export const SelectCity = (label) => {
     <div className="mb-6">
       <Label label={label} />
       <div className="inline-block relative w-64">
-        <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-          <option>Selecione a cidade</option>
-          {city.map((item) => (
-            <option key={item.id}>{item.name_ptbr}</option>
-          ))}
-        </select>
+        <Select
+          isMulti
+          options={city}
+          getOptionValue={(option) => option.code}
+          getOptionLabel={(option) => option.name}
+        />
       </div>
     </div>
   )
@@ -22,20 +23,17 @@ export const SelectCity = (label) => {
 
 export const SelectCountry = (label) => {
   const country = useCountry('country')
-  console.log(country[0])
 
   return (
     <div className="mb-6">
       <Label label={label} />
       <div className="inline-block relative w-64">
-        <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-          <option>Selecione o país</option>
-          {country.map((item) => (
-            <option key={item.code} value={item.name}>
-              {item.name_ptbr}
-            </option>
-          ))}
-        </select>
+        <Select
+          isMulti
+          options={country}
+          getOptionValue={(option) => option.code}
+          getOptionLabel={(option) => option.name}
+        />
       </div>
     </div>
   )
